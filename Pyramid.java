61955b75e28d6eef37c5b4c44137b89e4cff7a00
anchalgithub-2002 <109715107+anchalgithub-2002@users.noreply.github.com>
2022-07-28 10:23:48 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,14 @@
+public class Pyramid
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=1;j++)
+			{
+				System.out.println("*");
+			}
+		System.out.println(); //new line
+		}
+	}
+}
